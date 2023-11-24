2115c5a9bd7b5433605096a8a69ef185610e612b
amine alaoui <omari1amineco@gmail.com>
2023-11-24 20:58:28 +0100

    feat: add google logo image config to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ["www.google.co.uk"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
