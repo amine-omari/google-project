ba47e9f778734cb0df4cd83676113d44cddff8ab
amine alaoui <omari1amineco@gmail.com>
2023-11-24 21:01:03 +0100

    feat: add google image to BodyBar component using next Image

--- src/components/BodyBar.jsx
+++ src/components/BodyBar.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 const BodyBar = () => {
   return (
-    <div>BodyBar</div>
-  )
-}
+    <form>
+      <Image
+        src="https://www.google.co.uk/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+        alt="googlelogo"
+        height={100}
+        width={300}
+      />
+    </form>
+  );
+};
 
-export default BodyBar
+export default BodyBar;
