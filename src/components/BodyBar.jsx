43e50455e96c60d93cc8b01044c075b5567f0e1b
amine alaoui <omari1amineco@gmail.com>
2023-11-24 23:14:43 +0100

    fix: BodyBar component

--- src/components/BodyBar.jsx
+++ src/components/BodyBar.jsx
@@ -17,26 +17,24 @@ const BodyBar = () => {
   ];
 
   return (
-    <div>
-      <form className="flex flex-col items-center mt-44 flex-grow">
-        <Image
-          src="https://www.google.co.uk/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
-          alt="googlelogo"
-          height={100}
-          width={300}
-        />
-        <div className="flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md rounded-full border border-gray-200 px-5 py-3 items-center sm:max-w-xl lg:max-w-2xl">
-          <Search styles="h-5 mr-3 text-gray-500" />
-          <input type="text" className="flex-grow focus:outline-none" />
-          <Microphone styles="h-5" />
-        </div>
-        <div className="flex flex-col w-1/2 space-y-2 justify-center mt-8 sm:space-y-0 sm:flex-row sm: space-x-4">
-          {BODY_BUTTONS.map(({ id, textButtons }) => (
-            <BodyButtons key={id} textButton={textButtons} />
-          ))}
-        </div>
-      </form>
-    </div>
+    <form className="flex flex-col items-center mt-44 flex-grow">
+      <Image
+        src="https://www.google.co.uk/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+        alt="googlelogo"
+        height={100}
+        width={300}
+      />
+      <div className="flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md rounded-full border border-gray-200 px-5 py-3 items-center sm:max-w-xl lg:max-w-2xl">
+        <Search styles="h-5 mr-3 text-gray-500" />
+        <input type="text" className="flex-grow focus:outline-none" />
+        <Microphone styles="h-5" />
+      </div>
+      <div className="flex flex-col w-1/2 space-y-2 justify-center mt-8 sm:space-y-0 sm:flex-row sm: space-x-4">
+        {BODY_BUTTONS.map(({ id, textButtons }) => (
+          <BodyButtons key={id} textButton={textButtons} />
+        ))}
+      </div>
+    </form>
   );
 };
 
