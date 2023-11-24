e600bbb011f512c3452ee21ea15334feb5cb49c5
amine alaoui <omari1amineco@gmail.com>
2023-11-24 23:18:42 +0100

    fix: BodyBar commponent

--- src/components/BodyBar.jsx
+++ src/components/BodyBar.jsx
@@ -17,7 +17,7 @@ const BodyBar = () => {
   ];
 
   return (
-    <form className="flex flex-col items-center mt-44 flex-grow">
+    <form className="flex flex-col items-center mt-44 flex-grow w-4/5">
       <Image
         src="https://www.google.co.uk/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
         alt="googlelogo"
@@ -29,7 +29,7 @@ const BodyBar = () => {
         <input type="text" className="flex-grow focus:outline-none" />
         <Microphone styles="h-5" />
       </div>
-      <div className="flex flex-col w-1/2 space-y-2 justify-center mt-8 sm:space-y-0 sm:flex-row sm: space-x-4">
+      <div className="flex flex-col w-1/2 space-y-2 justify-center mt-8 sm:space-y-0 sm:flex-row sm:space-x-4">
         {BODY_BUTTONS.map(({ id, textButtons }) => (
           <BodyButtons key={id} textButton={textButtons} />
         ))}
