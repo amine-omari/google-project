87cd28d9108fd8b9281244102143a23e206df6e3
amine alaoui <omari1amineco@gmail.com>
2023-11-24 22:21:03 +0100

    feat: import BodyButtons component to BodyBar component and make it reusable

--- src/components/BodyBar.jsx
+++ src/components/BodyBar.jsx
@@ -2,20 +2,37 @@ import Microphone from "@/icons/Microphone";
 import Search from "@/icons/Search";
 import Image from "next/image";
 import React from "react";
+import BodyButtons from "./BodyButtons";
 
 const BodyBar = () => {
+  const BODY_BUTTONS = [
+    {
+      id: 1,
+      textButtons: "Google Search",
+    },
+    {
+      id: 2,
+      textButtons: "I'm Feeling Lucky",
+    },
+  ];
+
   return (
-    <form>
+    <form className="flex flex-col items-center mt-44 flex-grow">
       <Image
         src="https://www.google.co.uk/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
         alt="googlelogo"
         height={100}
         width={300}
       />
-      <div className="flex w-full mt-5 hover:shadow-lg">
+      <div className="flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md rounded-full border border-gray-200 px-5 py-3 items-center sm:max-w-xl lg:max-w-2xl">
         <Search styles="h-5 mr-3 text-gray-500" />
-        <input type="text" className="bg-red-500 focus:outline-none" />
+        <input type="text" className="flex-grow focus:outline-none" />
         <Microphone styles="h-5" />
+      </div>
+      <div>
+        {BODY_BUTTONS.map(({ id, textButtons }) => (
+          <BodyButtons key={id} textButton={textButtons} />
+        ))}
       </div>
     </form>
   );
