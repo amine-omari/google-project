cdbea983e063fffb1b7123cb44a7ccf351ecce8c
amine alaoui <omari1amineco@gmail.com>
2023-11-24 20:24:25 +0100

    feat: import BodyBar component to page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
+import BodyBar from "@/components/BodyBar";
 import HeaderBar from "@/components/HeaderBar";
 
 export default function Home() {
   return (
     <div>
       <HeaderBar />
+      <BodyBar />
     </div>
   );
 }
