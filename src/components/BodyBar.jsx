f0e869ff3655083c89fa8b153a1cb867c37c8cf7
amine alaoui <omari1amineco@gmail.com>
2023-11-24 21:27:44 +0100

    feat: import Search icon to BodyBar component

--- src/components/BodyBar.jsx
+++ src/components/BodyBar.jsx
@@ -1,3 +1,4 @@
+import Search from "@/icons/Search";
 import Image from "next/image";
 import React from "react";
 
@@ -8,8 +9,12 @@ const BodyBar = () => {
         src="https://www.google.co.uk/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
         alt="googlelogo"
         height={100}
-        width={300} 
+        width={300}
       />
+      <div>
+        <Search styles="h-5 mr-3 text-gray-500" />
+        <input type="text" className="focus:outline-none" />
+      </div>
     </form>
   );
 };
