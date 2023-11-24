655c7350d3941c5841f42e27b9bb5f18d77c364b
amine alaoui <omari1amineco@gmail.com>
2023-11-24 16:59:23 +0100

    fix: Avatar type of the file

--- src/components/Avatar.js
+++
@@ -1,7 +0,0 @@
-function Avatar({url}) {
-  return (
-    <img loading="lazy" src={url} alt="profile pic" className="h-10 rounded-full cursor-pointer transition transform duration hover:scale-110" />
-  )
-}
-
-export default Avatar
