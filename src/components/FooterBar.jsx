6c87bb5425be7acab17647a5b1209e9453d86be7
amine alaoui <omari1amineco@gmail.com>
2023-11-25 16:31:36 +0100

    feat: import Globe icon to FooterBar component

--- src/components/FooterBar.jsx
+++ src/components/FooterBar.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import Globe from "@/icons/Globe";
+import React from "react";
 
 const FooterBar = () => {
   return (
-    <div>FooterBar</div>
-  )
-}
+    <footer className="grid-w-full divide-y-[1px] divide-gray-300">
+      <div className="px-8 py-3">
+        <p>United Kingdom</p>
+      </div>
+      <div>
+        <div>
+          <Globe styles="h-5 mr-1 text-green-700" /> Carboon neutral since 2007
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-export default FooterBar
+export default FooterBar;
