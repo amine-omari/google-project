34f84107454007aa63aef40fe76ce974805c6002
amine alaoui <omari1amineco@gmail.com>
2023-11-26 22:51:14 +0100

    feat: add new Sun icon

--- src/icons/Sun.jsx
+++ src/icons/Sun.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React from "react";
 
-const Sun = () => {
+const Sun = ({ styles }) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -8,7 +8,7 @@ const Sun = () => {
       viewBox="0 0 24 24"
       strokeWidth={1.5}
       stroke="currentColor"
-      className="w-6 h-6"
+      className={styles}
     >
       <path
         strokeLinecap="round"
@@ -17,6 +17,6 @@ const Sun = () => {
       />
     </svg>
   );
-}
+};
 
-export default Sun
+export default Sun;
