cc8d8ea6c50abebe348511fed374461df94cbcd8
amine alaoui <omari1amineco@gmail.com>
2023-11-26 23:26:32 +0100

    feat: add darkmode style to FooterBar component

--- src/components/FooterBar.jsx
+++ src/components/FooterBar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const FooterBar = () => {
   return (
-    <footer className="grid w-full divide-y-[1px] divide-gray-200 bg-gray-100 text-sm text-gray-500">
+    <footer className="grid w-full divide-y-[1px] divide-neutral-500 bg-gray-100 text-sm text-gray-500 dark:bg-neutral-700 dark:text-gray-200">
       <div className="px-8 py-3">
         <p>United Kingdom</p>
       </div>
