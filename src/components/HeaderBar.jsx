2f4178616a9099c0dfb26d81033be8b92999e101
amine alaoui <omari1amineco@gmail.com>
2023-11-27 11:45:51 +0100

    fix: HeaderBar component

--- src/components/HeaderBar.jsx
+++ src/components/HeaderBar.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import Menu from "@/icons/Menu";
 import Avatar from "./Avatar";
 
-const HeaderBar = () => {
+const HeaderBar = ({ children }) => {
   return (
-    <header className="flex w-full p-5 justify-between text-sm text-gray-700">
+    <header className="dark:text-white flex w-full p-5 justify-between text-sm text-gray-700">
       {/* LEFT SECTION */}
       <div className="flex space-x-4 items-center">
         <p className="link">About</p>
@@ -13,10 +13,11 @@ const HeaderBar = () => {
 
       {/* RIGHT SECTION */}
       <div className="flex space-x-4 items-center">
+        {children}
         <p className="link">Gmail</p>
         <p className="link">Images</p>
 
-        <Menu styles="h-10 w-10 p-2 rounded-full hover:bg-gray-100 cursor-pointer" />
+        <Menu styles="h-10 w-10 p-2 rounded-full hover:bg-gray-100 cursor-pointer dark:hover:bg-neutral-600" />
 
         <Avatar url="https://avatars.githubusercontent.com/u/108984827?v=4" />
       </div>
