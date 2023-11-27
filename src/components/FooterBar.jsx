ed13e44f0de8df55f6d59a70af21b57c73de0e55
amine alaoui <omari1amineco@gmail.com>
2023-11-27 13:13:19 +0100

    fix: FooterBar component

--- src/components/FooterBar.jsx
+++ src/components/FooterBar.jsx
@@ -3,13 +3,13 @@ import React from "react";
 
 const FooterBar = () => {
   return (
-    <footer className="grid w-full divide-y-[1px] divide-neutral-500 bg-gray-100 text-sm text-gray-500 dark:bg-neutral-700 dark:text-gray-200">
+    <footer className="grid w-full divide-y-[1px] divide-neutral-500 bg-gray-100 text-sm text-gray-500 dark:divide-neutral-600 dark:bg-neutral-700/50 dark:text-gray-200">
       <div className="px-8 py-3">
         <p>United Kingdom</p>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-4 grid-flow-row-dense px-8 py-3">
         <div className="flex justify-center items-center md:col-span-2 lg:col-span-1 lg:col-start-2">
-          <Globe styles="h-5 mr-1 text-green-700" /> Carboon neutral since 2007
+          <Globe styles="h-5 mr-1 text-green-700 dark:text-gray-100" /> Carboon neutral since 2007
         </div>
         <div className="flex justify-center space-x-8 whitespace-nowrap md:justify-self-start">
           <p>Advertising</p>
