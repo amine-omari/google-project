b5850bb32fa9d5d3feedb1146798d8b16458b29b
amine alaoui <omari1amineco@gmail.com>
2023-11-28 10:55:28 +0100

    fix: FooterBar

--- src/components/FooterBar.jsx
+++ src/components/FooterBar.jsx
@@ -9,7 +9,8 @@ const FooterBar = () => {
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-4 grid-flow-row-dense px-8 py-3">
         <div className="flex justify-center items-center md:col-span-2 lg:col-span-1 lg:col-start-2">
-          <Globe styles="h-5 mr-1 text-green-700 dark:text-gray-100" /> Carboon neutral since 2007
+          <Globe styles="h-5 mr-1 text-green-700 dark:text-gray-100" /> Carboon
+          neutral since 2007
         </div>
         <div className="flex justify-center space-x-8 whitespace-nowrap md:justify-self-start">
           <p>Advertising</p>
